c53dcbcc91a8b6c412f19939dfae521e37605ffc
RachelDoehr <33264020+RachelDoehr@users.noreply.github.com>
2020-07-22 13:19:56 -0400

    wip

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -102,7 +102,7 @@ class ClassicalModels():
 
         def __train_one_lag(ll):
 
-            endog = self.train_val_df['BAAFFM']
+            endog = self.train_val_df['INDPRO']
             forecasts = {}
             
             # Get the number of initial training observations
@@ -136,7 +136,7 @@ class ClassicalModels():
         
             # Combine all forecasts into a dataframe
             forecasts = pd.DataFrame(forecasts.items(), columns=['sasdate', 't_forecast'])
-            actuals = self.train_val_df.tail(forecasts.shape[0])[['sasdate', 'BAAFFM']]
+            actuals = self.train_val_df.tail(forecasts.shape[0])[['sasdate', 'INDPRO']]
             actuals.columns = ['sasdate', 't_actual']
             self.SS_AR_forecasts = pd.merge(forecasts, actuals, on='sasdate', how='inner')
             self.SS_AR_forecasts['sasdate'] = pd.to_datetime(self.SS_AR_forecasts['sasdate'])
@@ -148,7 +148,7 @@ class ClassicalModels():
             }
             self.logger.info('completed training for AR model with lag: '+str(ll))
 
-        [__train_one_lag(lag_value) for lag_value in range(1, 13)]
+        [__train_one_lag(lag_value) for lag_value in range(1, 4)]
 
         # save dictionary of models to disk for later use
         pth = Path(self.models_path, 'AR_models').with_suffix('.pkl')
@@ -168,7 +168,7 @@ class ClassicalModels():
 
         def __train_one_lag(ll):
 
-            endog = self.train_val_df['BAAFFM']
+            endog = self.train_val_df['INDPRO']
             forecasts = {}
             
             # Get the number of initial training observations
@@ -185,8 +185,8 @@ class ClassicalModels():
                                         switching_variance=True,
                                         )
             
-            res = mod.fit(search_reps=20)
-            res_extended = MSARExtension(res) # pass the trained model to the custom out-of-sample prediction class given statsmodels has not implemented yet:
+            res = mod.fit()
+            res_extended = MSARExtension(res) # pass the trained model to a custom out-of-sample prediction class given statsmodels has not implemented yet:
             # https://github.com/statsmodels/statsmodels/blob/ebe5e76c6c8055dddb247f7eff174c959acc61d2/statsmodels/tsa/regime_switching/markov_switching.py#L702-L703
             yhat = res_extended.predict_out_of_sample()
 
@@ -203,12 +203,11 @@ class ClassicalModels():
                                         order=ll,
                                         switching_variance=True
                 )
-                res = mod.fit(search_reps=20)
+                res = mod.fit()
                 res_extended = MSARExtension(res)
                 yhat = res_extended.predict_out_of_sample()
 
                 # Save the new set of forecasts, inverse the scaler
-                print(scaler_y.inverse_transform(yhat.ravel())[0])
                 forecasts[self.train_val_df.iloc[t+1, 1]] = scaler_y.inverse_transform(yhat.ravel())[0]
                 # save the model at end of time series
                 if t == nobs-2:
@@ -217,7 +216,7 @@ class ClassicalModels():
         
             # Combine all forecasts into a dataframe
             forecasts = pd.DataFrame(forecasts.items(), columns=['sasdate', 't_forecast'])
-            actuals = self.train_val_df.tail(forecasts.shape[0])[['sasdate', 'BAAFFM']]
+            actuals = self.train_val_df.tail(forecasts.shape[0])[['sasdate', 'INDPRO']]
             actuals.columns = ['sasdate', 't_actual']
             self.Markov_fcasts = pd.merge(forecasts, actuals, on='sasdate', how='inner').dropna()
             self.Markov_fcasts['sasdate'] = pd.to_datetime(self.Markov_fcasts['sasdate'])
@@ -230,7 +229,7 @@ class ClassicalModels():
             }
             self.logger.info('completed training for Markov Switching AR model with lag: '+str(ll))
         
-        [__train_one_lag(lag_value) for lag_value in range(2, 6)]
+        [__train_one_lag(lag_value) for lag_value in range(3, 4)]
 
         # save dictionary of models to disk for later use
         pth = Path(self.models_path, 'MKV_models').with_suffix('.pkl')
@@ -248,7 +247,7 @@ class ClassicalModels():
         self.forecasts_exp = {}
         self.EXP_models = {}
 
-        endog = self.train_val_df['BAAFFM']
+        endog = self.train_val_df['INDPRO']
         forecasts = {}
         
         # Get the number of initial training observations
@@ -266,14 +265,14 @@ class ClassicalModels():
         res = mod.fit()
 
         # Save initial forecast
-        forecasts[self.train_val_df.iloc[n_init_training, 1]] = scaler_y.inverse_transform(res.predict(start=len(training_endog_preprocessed), end=len(training_endog_preprocessed)))[0]
+        forecasts[self.train_val_df.iloc[n_init_training-1, 1]] = scaler_y.inverse_transform(res.predict())[len(res.predict())-1]
         # Step through the rest of the sample
         for t in range(n_init_training, nobs-1):
         
             scaler_y = StandardScaler()
             # Update the results by appending the next observation
             endog_preprocessed = pd.DataFrame(scaler_y.fit_transform(endog.iloc[0:t+1].values.reshape(-1, 1))) # re fit
-
+            dates = pd.DataFrame(self.train_val_df.iloc[0:t+1, 1].values.reshape(-1, 1))
             mod = ExponentialSmoothing(endog_preprocessed,
                                     trend=None,
                                     seasonal=None
@@ -281,16 +280,16 @@ class ClassicalModels():
             res = mod.fit()
 
             # Save the new set of forecasts, inverse the scaler
-            forecasts[self.train_val_df.iloc[t+1, 1]] = scaler_y.inverse_transform(res.predict(start=len(training_endog_preprocessed), end=len(training_endog_preprocessed)))[0]
+            forecasts[self.train_val_df.iloc[t, 1]] = scaler_y.inverse_transform(res.predict())[len(res.predict())-1]
             # save the model at end of time series
-            if t == nobs-2:
+            if t == nobs-1:
                 self.EXP_models['exp_weigh_lag_struct'] = res
                 self.standard_scaler_Expsmooth = scaler_y
         
         # Combine all forecasts into a dataframe
         forecasts = pd.DataFrame(forecasts.items(), columns=['sasdate', 't_forecast'])
-        actuals = self.train_val_df.tail(forecasts.shape[0])[['sasdate', 'BAAFFM']]
-        actuals.columns = ['sasdate', 't_actual']
+        actuals = pd.concat([endog.tail(forecasts.shape[0]), dates.tail(forecasts.shape[0])], axis=1)
+        actuals.columns = ['t_actual', 'sasdate']
         self.Expsmooth_fcasts = pd.merge(forecasts, actuals, on='sasdate', how='inner').dropna()
         self.Expsmooth_fcasts['sasdate'] = pd.to_datetime(self.Expsmooth_fcasts['sasdate'])
         
@@ -310,60 +309,6 @@ class ClassicalModels():
         pth = Path(self.models_path, 'scaler_ExponSmooth').with_suffix('.pkl')
         with open(pth, 'wb') as handle:
             pickle.dump(self.standard_scaler_Expsmooth, handle)
-
-    def train_ss_DFM(self):
-    
-        '''Trains Dynamic Factor model, including walk forward validation and standardization.
-        Optimizes lag number & factor number by calculating RMSE on validation set during walk-forward training.'''
-        self.error_metrics_DFM = {}
-        self.forecasts_DFM = {}
-
-        for ll in range(1, 3):
-
-            endog = self.train_val_df[['BAAFFM', 'INDPRO']]
-            forecasts = {}
-
-            # Get the number of initial training observations
-            nobs = len(endog)
-            n_init_training = int(nobs * 0.8)
-            scaler = StandardScaler()
-
-            # Create model for initial training sample, fit parameters
-            training_endog = endog.iloc[:n_init_training, :]
-            training_endog_preprocessed = pd.DataFrame(scaler.fit_transform(training_endog.values))
-            mod = sm.tsa.DynamicFactor(training_endog_preprocessed, k_factors=1, factor_order=ll) ######## CHECK IF YOU NEED ERROR ORDER TOO
-
-            res = mod.fit(low_memory=True, disp=False)
-
-            # Save initial forecast
-            forecasts[self.train_val_df.iloc[n_init_training-1, 1]] = scaler.inverse_transform(res.predict())[len(res.predict())-1][0]
-
-            # Step through the rest of the sample
-            for t in range(n_init_training, nobs):
-                # Update the results by appending the next observation
-                endog_preprocessed = pd.DataFrame(scaler.fit_transform(endog.iloc[:t+1, :].values)) # re fit
-                dates = pd.DataFrame(self.train_val_df.iloc[0:t+1, 1].values.reshape(-1, 1))
-    
-                mod = sm.tsa.DynamicFactor(endog_preprocessed, k_factors=1, factor_order=ll)
-                res = mod.fit(low_memory=True, disp=False)
-                # Save the new set of forecasts, inverse the scaler
-                forecasts[self.train_val_df.iloc[t, 1]] = scaler.inverse_transform(res.predict())[len(res.predict())-1][0]
-        
-            # Combine all forecasts into a dataframe
-            forecasts = pd.DataFrame(forecasts.items(), columns=['sasdate', 't_forecast'])
-
-            actuals = pd.concat([endog.tail(forecasts.shape[0]), dates.tail(forecasts.shape[0])], axis=1)
-            actuals = actuals[['BAAFFM', 0]]
-            actuals.columns = ['t_actual', 'sasdate']
-            self.SS_DFM_forecasts = pd.merge(forecasts, actuals, on='sasdate', how='inner')
-            self.SS_DFM_forecasts['sasdate'] = pd.to_datetime(self.SS_DFM_forecasts['sasdate'])
-            # error storage
-            self.error_metrics_DFM[ll] = mean_squared_error(self.SS_DFM_forecasts['t_actual'], self.SS_DFM_forecasts['t_forecast'])
-            # forecast storage
-            self.forecasts_DFM['lag_'+str(ll)] = {
-                'df': self.SS_DFM_forecasts
-            }
-            self.logger.info('completed training for DFM model with order: '+str(ll)+' and 2 factors')
 
     def plot_errors_AR(self):
 
@@ -406,52 +351,29 @@ class ClassicalModels():
         fig.savefig(pth)
         self.logger.info('plotted and saved png file in /reports/figures of Markov errors at various parameters')
 
-    def plot_errors_DFM(self):
-        
-        '''For validation / lag tuning, plots the errors of the different lag terms + switching variance/constant once trained'''
-        df_error = pd.DataFrame(self.error_metrics_DFM.items())
-        
-        fig, ax1 = plt.subplots(1,1, figsize=(16,9), dpi= 80)
-        plt.scatter(df_error.iloc[:, 0], df_error.iloc[:, 1], color='blue', s=10)
-        
-        # Decorations
-        ax1.set_xlabel('Lag', fontsize=20)
-        ax1.tick_params(axis='x', rotation=0, labelsize=12)
-        ax1.set_ylabel('Validation Set RMSE', color='black', fontsize=20)
-        ax1.tick_params(axis='y', rotation=0, labelcolor='black' )
-        fig.tight_layout()
-        plt.legend()
-        plt.title('Error Metrics: Dynamic Factor Model', fontsize=12, fontweight='bold')
-        
-        pth = Path(self.graphics_path, 'DFM_errors').with_suffix('.png')
-        fig.savefig(pth)
-        self.logger.info('plotted and saved png file in /reports/figures of Dynamic Factor Model errors at various parameters')
-
-    def plot_forecasts_Classical(self, chosen_lag_AR, chosen_lag_Markov, chosen_lag_DFM):
+    def plot_forecasts_Classical(self, chosen_lag_AR, chosen_lag_Markov):
         
         '''Plots and reports forecats (t+1) for both classical models'''
-        #df_Markov = self.forecasts_Markov['lag_'+str(chosen_lag_Markov)]['df']
+        df_Markov = self.forecasts_Markov['lag_'+str(chosen_lag_Markov)]['df']
         df_AR = self.forecasts_AR['lag_'+str(chosen_lag_AR)]['df']
         df_ExpSmo = self.forecasts_exp['exp_weigh_lag_struct']['df']
-        #df_DFM = self.forecasts_DFM['lag_'+str(chosen_lag_DFM)]['df']
 
         # Plot Line1 (Left Y Axis)
         fig, ax1 = plt.subplots(1,1, figsize=(16,9), dpi= 80)
         
         ax1.plot(df_AR['sasdate'], df_AR['t_actual'], color='dodgerblue', label='actual')
         ax1.plot(df_AR['sasdate'], df_AR['t_forecast'], color='navy', label=('state space AR, lag='+str(chosen_lag_AR)+' forecast'), linestyle=":")
-        #ax1.plot(df_Markov['sasdate'], df_Markov['t_forecast'], color='crimson', label=('Markov switching model, lag='+str(chosen_lag_Markov)+' forecast'), linestyle=":")
+        ax1.plot(df_Markov['sasdate'], df_Markov['t_forecast'], color='crimson', label=('Markov switching model, lag='+str(chosen_lag_Markov)+' forecast'), linestyle=":")
         ax1.plot(df_ExpSmo['sasdate'], df_ExpSmo['t_forecast'], color='gray', label=('exponential smoothing model forecast'), linestyle=":")
-        #ax1.plot(df_DFM['sasdate'], df_DFM['t_forecast'], color='lightskyblue', label=('dynamic factor model, lag='+str(chosen_lag_DFM)+' forecast'), linestyle=":")
 
         # Decorations
         # ax1 (left Y axis)
         ax1.set_xlabel('Date', fontsize=20)
         ax1.tick_params(axis='x', rotation=0, labelsize=12)
-        ax1.set_ylabel('BAAFFM Spread', color='black', fontsize=20)
+        ax1.set_ylabel('Industrial Production', color='black', fontsize=20)
         ax1.tick_params(axis='y', rotation=0, labelcolor='black' )
         ax1.grid(alpha=.4)
-        plt.title('Forecast vs. Realized Values: BAAFFM', weight='bold')
+        plt.title('Forecast vs. Realized Values: INDPRO', weight='bold')
 
         fig.tight_layout()
         plt.legend()
@@ -463,13 +385,12 @@ class ClassicalModels():
         self.get_data()
         self.splice_test_data()
         #self.examine_autocorr_stationary()
-        #self.train_ss_AR()
-        #self.plot_errors_AR()
+        self.train_ss_AR()
+        self.plot_errors_AR()
         self.train_MarkovSwitch_AR()
         self.plot_errors_Markov()
-        #self.train_exponential_smoother()
-        #self.train_ss_DFM()
-        #self.plot_forecasts_Classical(chosen_lag_AR=9, chosen_lag_Markov=6, chosen_lag_DFM=2)
+        self.train_exponential_smoother()
+        self.plot_forecasts_Classical(chosen_lag_AR=3, chosen_lag_Markov=3)
 
 
 def main():
